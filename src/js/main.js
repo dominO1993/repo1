3601781e6186e4b0255cbb700b68f67d12626139
Dominik <dominixp93@gmail.com>
2022-11-18 12:31:02 +0100

    added handle nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 const burgerBtn = document.querySelector('.burger-btn');
 const navList = document.querySelector('.nav__list');
+const allNavItems = document.querySelectorAll('.nav__item');
 
 const handleNav = () => {
 	navList.classList.toggle('nav__list--active');
 };
+
+allNavItems.forEach(item => {
+	item.addEventListener('click', () => {
+		navList.classList.remove('nav__list--active');
+	});
+});
 
 burgerBtn.addEventListener('click', handleNav);
