88d6f6c0f85e89124260f154ac8677e33c10ab07
Dominik <dominixp93@gmail.com>
2022-12-05 12:42:26 +0100

    added google maps and create form js

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,16 @@ const navList = document.querySelector('.nav__list');
 const allNavItems = document.querySelectorAll('.nav__item');
 const footerYear = document.querySelector('.footer__year');
 
+const firstName = document.querySelector('#first-name');
+const secondName = document.querySelector('#second-name');
+const email = document.querySelector('#email');
+const phoneNumber = document.querySelector('#phone-number');
+const formMessage = document.querySelector('#message');
+
+const errorFormText = document.querySelector('.error');
+const resetFormBtn = document.querySelector('.clear');
+const sendFormBtn = document.querySelector('.send');
+
 const handleNav = () => {
 	navList.classList.toggle('nav__list--active');
 };
@@ -21,11 +31,32 @@ const handleCurrentYear = () => {
 
 handleCurrentYear();
 
+const showError = () => {
+errorFormText.classList.add('error--show')
+};
+
+const checkForm = input => {
+	input.forEach(el => {
+		if (el.value === '') {
+			showError();
+		} else {
+			console.log('ok');
+		}
+	});
+};
+
+sendFormBtn.addEventListener('click', e => {
+	e.preventDefault();
+
+	checkForm([firstName, secondName, email, phoneNumber, formMessage]);
+});
+
+resetFormBtn.addEventListener('click', e => {
+	e.preventDefault();
+
+	[firstName, secondName, email, phoneNumber, formMessage].forEach(el => {
+		el.value = '';
+	});
+});
+
 burgerBtn.addEventListener('click', handleNav);
-// window.addEventListener('scroll', () => {
-// 	if (window.scrollY > 200) {
-// 		nav.classList.add('nav--transparent');
-// 	} else {
-// 		nav.classList.remove('nav--transparent');
-// 	}
-// });
